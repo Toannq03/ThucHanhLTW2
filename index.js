2503522da03f32091117039f5d6853dcb253210c
Toannq03 <160617237+Toannq03@users.noreply.github.com>
2024-05-08 10:04:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,25 @@ for (let i = 0; i < accordian.length; i++) {
     }
   });
 }
+
+// Lấy các phần tử cần thiết
+const signinButton = document.querySelector('.signin__button');
+const modal = document.getElementById('login-modal');
+const span = document.getElementsByClassName('close')[0];
+
+// Thêm sự kiện click cho nút đăng nhập
+signinButton.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+// Thêm sự kiện click cho nút đóng modal
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// Thêm sự kiện click bên ngoài modal để đóng modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
