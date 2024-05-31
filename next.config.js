12e57f32a144fc7339aa886b4757a9e31a6694d4
Toannq03 <160617237+Toannq03@users.noreply.github.com>
2024-05-31 09:25:21 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
